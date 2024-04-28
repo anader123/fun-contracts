de1265c777a0a647984d4ea1494772fd47b31417
Nader <alannaderdev@gmail.com>
2024-04-28 10:00:47 -0600

    update highest price auction tests

--- src/Test721.sol
+++ src/Test721.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Test721 is ERC721 {
+    uint256 private _nextTokenId = 1;
     constructor() ERC721("Test", "TST") {}
 
     function mint() public {
-        _mint(msg.sender, 1);
+        uint256 tokenId = _nextTokenId++;
+        _mint(msg.sender, tokenId);
     }
 }
--- test/HighestPriceAuction.t.sol
+++ test/HighestPriceAuction.t.sol
@@ -21,6 +21,22 @@ contract HighestPriceAuctionTest is Test {
         vm.stopPrank();
     }
 
+    function testCreateAuction() public {
+        vm.startPrank(address(777));
+        nftContract.mint();
+
+        highestPriceAuction.createAuction(address(nftContract), 2, uint32(block.timestamp + 1 days));
+        (address seller , , , , , , , ) = highestPriceAuction.auctions(1);
+
+        assertEq(seller, address(777));
+        vm.stopPrank();
+    }
+
+    function testStart() public view {
+        address nftOwner = nftContract.ownerOf(1);
+        assertEq(nftOwner, address(highestPriceAuction));
+    }
+
     function testBid() public {
         (, , , , , , address highestBidder1 , ) = highestPriceAuction.auctions(0);
         assertEq(highestBidder1, address(0));
@@ -36,13 +52,23 @@ contract HighestPriceAuctionTest is Test {
         hoax(address(321), 1 ether);
         highestPriceAuction.bid{value: 1 ether}(0);
 
+        uint256 beforeSale = address(123).balance;
+
         vm.warp(block.timestamp + 2 days);
         highestPriceAuction.end(0);
+        address newOwner = nftContract.ownerOf(1);
+        assertEq(newOwner, address(321));
+
+        uint256 afterSale = address(123).balance;
+        assertEq(beforeSale + 1 ether, afterSale);
     }
 
     function testEndWithOutBid() public {
         vm.warp(block.timestamp + 2 days);
         highestPriceAuction.end(0);
+
+        address newOwner = nftContract.ownerOf(1);
+        assertEq(newOwner, address(123));
     }
 
 }
