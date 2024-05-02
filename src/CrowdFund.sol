f8e64f0846469205d8ba1d327256d4e9b411a405
Nader <alannaderdev@gmail.com>
2024-05-02 09:44:05 -0600

    update crowdfund to accept any token

--- src/CrowdFund.sol
+++ src/CrowdFund.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CrowdFund {
-    event Launch(uint256 id, address indexed creator, uint256 goal, uint32 startAt, uint32 endAt);
+    event Launch(
+        uint256 id, address indexed creator, address indexed token, uint256 goal, uint32 startAt, uint32 endAt
+    );
     event Cancel(uint256 id);
     event Pledge(uint256 id, address indexed caller, uint256 amount);
     event Unpledge(uint256 id, address indexed caller, uint256 amount);
@@ -13,6 +15,7 @@ contract CrowdFund {
 
     struct Campaign {
         address creator;
+        address token;
         uint256 goal;
         uint256 pledged;
         uint32 startAt;
@@ -20,25 +23,28 @@ contract CrowdFund {
         bool claimed;
     }
 
-    IERC20 public immutable token;
     uint256 public count;
 
     mapping(uint256 => Campaign) public campaigns;
     mapping(uint256 => mapping(address => uint256)) public pledgedAmount;
 
-    constructor(address _token) {
-        token = IERC20(_token);
-    }
-
-    function launch(uint256 goal, uint32 startAt, uint32 endAt) external {
+    function launch(address token, uint256 goal, uint32 startAt, uint32 endAt) external {
         require(startAt >= block.timestamp, "start at < now");
         require(endAt >= startAt, "end at < start at");
         require(endAt <= block.timestamp + 90 days, "end at > max duration");
 
         count += 1;
-        campaigns[count] = Campaign({creator: msg.sender, goal: goal, pledged: 0, startAt: startAt, endAt: endAt, claimed: false});
+        campaigns[count] = Campaign({
+            creator: msg.sender,
+            token: token,
+            goal: goal,
+            pledged: 0,
+            startAt: startAt,
+            endAt: endAt,
+            claimed: false
+        });
 
-        emit Launch(count, msg.sender, goal, startAt, endAt);
+        emit Launch(count, msg.sender, token, goal, startAt, endAt);
     }
 
     function cancel(uint256 id) external {
@@ -57,6 +63,8 @@ contract CrowdFund {
 
         campaign.pledged += amount;
         pledgedAmount[id][msg.sender] += amount;
+
+        IERC20 token = IERC20(campaign.token);
         token.transferFrom(msg.sender, address(this), amount);
 
         emit Pledge(id, msg.sender, amount);
@@ -69,6 +77,8 @@ contract CrowdFund {
 
         campaign.pledged -= amount;
         pledgedAmount[id][msg.sender] -= amount;
+
+        IERC20 token = IERC20(campaign.token);
         token.transfer(msg.sender, amount);
 
         emit Unpledge(id, msg.sender, amount);
@@ -82,6 +92,7 @@ contract CrowdFund {
         require(!campaign.claimed, "claimed");
 
         campaign.claimed = true;
+        IERC20 token = IERC20(campaign.token);
         token.transfer(msg.sender, campaign.pledged);
 
         emit Claim(id);
@@ -94,6 +105,8 @@ contract CrowdFund {
 
         uint256 bal = pledgedAmount[id][msg.sender];
         pledgedAmount[id][msg.sender] = 0;
+
+        IERC20 token = IERC20(campaign.token);
         token.transfer(msg.sender, bal);
 
         emit Refund(id, msg.sender, bal);
